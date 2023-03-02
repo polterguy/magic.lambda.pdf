71774a7a016ea0fc7aa01f180a7b24a54db732e2
Thomas Hansen <th@aista.com>
2023-03-02 13:10:33 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# magic.lambda.pdf
-PDF helper slots for Magic and Hyperlambda
+
+# magic.lambda.config - Accessing your server's configuration from Hyperlambda
+
+This project provides configuration settings slots for Magic. The project provides the following
+slots, allowing you to retrieve configuration settings from your _"appsettings.json"_ configuration file,
+in addition to saving and loading your configuration file.
+
+* __[config.get]__ - Returns a configuration value from your configuration file with the specified key
+* __[config.section]__ - Returns the specified configuration section from your configuration file with the specified key
+* __[config.load]__ - Loads your _"appsettings.json"_ file and returns as a string
+* __[config.save]__ - Saves your _"appsettings.json"_ file, sanity checking it's valid JSON before persisting your file
+
+## How to use [config.get]
+
+This slot allows you to retrieve configuration settings. To retrieve settings you can supply a
+_"path"_ such as _"foo:bar"_ to for instance an invocation to **[config.get]**. This will traverse into
+your _"foo"_ config setting, find its _"bar"_ key, and return the value of your _"bar"_ key. Below is
+an example of usage.
+
+```
+config.get:"foo:bar"
+```
+
+Assuming your configuration file looks like the following.
+
+```
+{
+   "foo": {
+      "bar": 42
+   }
+}
+```
+
+... afterwards the value of your __[config.get]__ node will be the following.
+
+```
+config.get:42
+```
+
+**Notice** - Due to implementation details of .Net and its `IConfiguration` specifically, values
+returned will _always_ be strings, and you'll have to manually convert these to other types, using for
+instance the **[convert]** slot from magic.lambda. This _might_ change in a future release though.
+You can also provide a default value that will be returned if no configuration value is found, such
+as the following illustrates.
+
+```
+.foo:foo
+config.get:"magic:foo:non-existing-key"
+   get-value:x:@.foo
+```
+
+Since the above _"non-existing-key"_ doesn't exist in your configuration file, the above Hyperlambda will
+return the value _"foo"_ being the result of the invocation to the **[get-value]** slot. This allows you
+to create default values your code resorts to if the specified configuration setting doesn't exist.
+
+## Project website for magic.lambda.config
+
+The source code for this repository can be found at [github.com/polterguy/magic.lambda.config](https://github.com/polterguy/magic.lambda.config), and you can provide feedback, provide bug reports, etc at the same place.
+
+- ![Build status](https://github.com/polterguy/magic.lambda.config/actions/workflows/build.yaml/badge.svg)
+- [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=alert_status)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Bugs](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=bugs)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=code_smells)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Coverage](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=coverage)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Duplicated Lines (%)](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=duplicated_lines_density)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Lines of Code](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=ncloc)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=sqale_rating)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Reliability Rating](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=reliability_rating)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Security Rating](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=security_rating)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Technical Debt](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=sqale_index)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
+- [![Vulnerabilities](https://sonarcloud.io/api/project_badges/measure?project=polterguy_magic.lambda.config&metric=vulnerabilities)](https://sonarcloud.io/dashboard?id=polterguy_magic.lambda.config)
---
+++ magic.lambda.pdf/Pdf2Text.cs
@@ -0,0 +1,195 @@
+﻿/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+using System.IO;
+using System.Text;
+using iText.Kernel.Pdf;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.pdf
+{
+    /// <summary>
+    /// [pdf2text] slot for retrieving a configuration key.
+    /// </summary>
+    [Slot(Name = "pdf2text")]
+    public class Pdf2Text : ISlot
+    {
+        private static int _numberOfCharsToKeep = 15;
+
+        /// <summary>
+        /// Implementation of your slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal your slot.</param>
+        /// <param name="input">Arguments to your slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = ExtractText(input.GetEx<byte[]>());
+        }
+
+        #region [ -- Private helper methods -- ]
+
+        string ExtractText(byte[] bytes)
+        {
+            var builder = new StringBuilder();
+            using (var memStream = new MemoryStream())
+            {
+                memStream.Write(bytes);
+                memStream.Position = 0;
+                var reader = new PdfReader(memStream);
+                var doc = new PdfDocument(reader);
+
+                int totalLen = 68;
+                float charUnit = ((float)totalLen) / (float)doc.GetNumberOfPages();
+                int totalWritten = 0;
+                float curUnit = 0;
+
+                for (int page = 1; page <= doc.GetNumberOfPages(); page++)
+                {
+                    builder.Append(ExtractTextFromPDFBytes(doc.GetPage(page).GetContentBytes()) + " ");
+
+                    if (charUnit >= 1.0f)
+                    {
+                        for (int i = 0; i < (int)charUnit; i++)
+                        {
+                            totalWritten++;
+                        }
+                    }
+                    else
+                    {
+                        curUnit += charUnit;
+                        if (curUnit >= 1.0f)
+                        {
+                            for (int i = 0; i < (int)curUnit; i++)
+                            {
+                                totalWritten++;
+                            }
+                            curUnit = 0;
+                        }
+                    }
+                }
+
+            }
+            return builder.ToString();
+        }
+
+        bool CheckToken(string[] tokens, char[] recent)
+        {
+            foreach (string token in tokens)
+            {
+                if ((recent[_numberOfCharsToKeep - 3] == token[0]) &&
+                    (recent[_numberOfCharsToKeep - 2] == token[1]) &&
+                    ((recent[_numberOfCharsToKeep - 1] == ' ') ||
+                    (recent[_numberOfCharsToKeep - 1] == 0x0d) ||
+                    (recent[_numberOfCharsToKeep - 1] == 0x0a)) &&
+                    ((recent[_numberOfCharsToKeep - 4] == ' ') ||
+                    (recent[_numberOfCharsToKeep - 4] == 0x0d) ||
+                    (recent[_numberOfCharsToKeep - 4] == 0x0a)))
+                    return true;
+            }
+            return false;
+        }
+
+        string ExtractTextFromPDFBytes(byte[] input)
+        {
+            if (input == null || input.Length == 0)
+                return "";
+
+            string resultString = "";
+            bool inTextObject = false;
+            bool nextLiteral = false;
+            int bracketDepth = 0;
+            char[] previousCharacters = new char[_numberOfCharsToKeep];
+            for (int j = 0; j < _numberOfCharsToKeep; j++)
+            {
+                previousCharacters[j] = ' ';
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = (char)input[i];
+                if (inTextObject)
+                {
+                    if (bracketDepth == 0)
+                    {
+                        if (CheckToken(new string[] { "TD", "Td" }, previousCharacters))
+                        {
+                        resultString += "\n\r";
+                        }
+                        else
+                        {
+                            if (CheckToken(new string[] { "'", "T*", "\"" }, previousCharacters))
+                            {
+                                resultString += "\n";
+                            }
+                            else
+                            {
+                                if (CheckToken(new string[] { "Tj" }, previousCharacters))
+                                {
+                                    resultString += " ";
+                                }
+                            }
+                        }
+                    }
+
+                    // End of a text object, also go to a new line.
+                    if (bracketDepth == 0 && CheckToken(new string[] { "ET" }, previousCharacters))
+                    {
+                        inTextObject = false;
+                        resultString += " ";
+                    }
+                    else
+                    {
+                        // Start outputting text
+                        if ((c == '(') && (bracketDepth == 0) && (!nextLiteral))
+                        {
+                            bracketDepth = 1;
+                        }
+                        else
+                        {
+                            // Stop outputting text
+                            if ((c == ')') && (bracketDepth == 1) && (!nextLiteral))
+                            {
+                                bracketDepth = 0;
+                            }
+                            else
+                            {
+                                // Just a normal text character:
+                                if (bracketDepth == 1)
+                                {
+                                    // Only print out next character no matter what. 
+                                    // Do not interpret.
+                                    if (c == '\\' && !nextLiteral)
+                                    {
+                                        nextLiteral = true;
+                                    }
+                                    else
+                                    {
+                                        if (((c >= ' ') && (c <= '~')) ||
+                                            ((c >= 128) && (c < 255)))
+                                            resultString += c.ToString();
+                                        nextLiteral = false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int j = 0; j < _numberOfCharsToKeep - 1; j++)
+                {
+                    previousCharacters[j] = previousCharacters[j + 1];
+                }
+                previousCharacters[_numberOfCharsToKeep - 1] = c;
+
+                if (!inTextObject && CheckToken(new string[] { "BT" }, previousCharacters))
+                    inTextObject = true;
+            }
+            return resultString;
+        }
+
+        #endregion
+    }
+}
